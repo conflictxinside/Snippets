2dde1fc9fc1c82a0ebbb676c40e14679d2ff9c10
Warren G <conflictxinside@gmail.com>
2012-01-12 11:15:30 -0500

    Added

---
+++ yt_if_wmode_fix.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$('iframe').each(function(){
+		if($(this).attr('src').indexOf('youtube.com/embed') != -1) {
+			ythref = $(this).attr('src');
+			if(ythref.indexOf('?') == -1) {
+				$(this).attr('src', ythref + '?wmode=transparent');
+			} else {
+				$(this).attr('src', ythref + '&wmode=transparent');
+			}
+		}
+	});
+});
